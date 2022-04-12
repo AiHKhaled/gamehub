00162ae7997b329cc13cab6e4c4381a527507f6f
parzivalElijah <aithammouk40@gmail.com>
2022-04-12 14:01:02 +0100

    to a new branch

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import Navbar from "../components/Navbar";
 import { AuthContextProvider } from "../stores/authContext";
 import "../styles/globals.css";
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthContextProvider>
       <Navbar />
-      <Component {...pageProps} />
+      <Component {...pageProps} key={router.route} />
     </AuthContextProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,36 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+import { motion } from "framer-motion/dist/framer-motion";
+
+const variants = {
+  hidden: { scale: 0.8, opacity: 0 },
+  visible: { scale: 1, opacity: 1, transition: { delay: 3 } },
+};
 
 export default function Home() {
   return (
     <div className={styles.home}>
-       <h2>Welcome to Gaming Vibes</h2>
+      <motion.div
+        className="title"
+        initial="hidden"
+        animate="visible"
+        variants={variants}
+      >
+        <h2>Welcome to Gaming Vibes</h2>
+      </motion.div>
       <div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          deleniti rem aspernatur odit hic autem neque repellat alias? Debitis
+          veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor
+          id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          deleniti rem aspernatur odit hic autem neque repellat alias? Debitis
+          veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor
+          id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          deleniti rem aspernatur odit hic autem neque repellat alias? Debitis
+          veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor
+          id.
+        </p>
       </div>
     </div>
-  )
+  );
 }
